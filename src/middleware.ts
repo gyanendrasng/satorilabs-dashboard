bf713692c476145ee1bba06e59e517f7e177d2c0
Gyanendra Singh <gyanendra@gorinsystems.com>
2025-11-13 23:36:59 +0530

    fix matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -68,5 +68,15 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public assets
+     */
+    '/((?!backend|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$).*)',
+  ],
 };
